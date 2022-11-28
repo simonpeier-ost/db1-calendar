724a9515f023aed90517d9d7c341240611ed95d5
oliverdietsche <oliver.dietsche@gmail.com>
2022-11-28 09:06:01 +0100

    Testat 3 Aufgabe 1&2

--- dbs/3_inserts.sql
+++ dbs/3_inserts.sql
@@ -9,7 +9,8 @@ INSERT INTO person values
 (4, 'Sayed', 'Lu', 'sayed.lu@ost.ch', NULL),
 (5, 'Antony', 'Sexton', 'antony.sexton@ost.ch', to_date('2007-02-16', 'YYYY-MM-DD')),
 (6, 'Yannis', 'Derrick', 'yannis.derrick@ost.ch', to_date('2010-10-10', 'YYYY-MM-DD')),
-(7, 'Abdi', 'Randall', 'abdi.randall@ost.ch', to_date('2020-05-05', 'YYYY-MM-DD'));
+(7, 'Abdi', 'Randall', 'abdi.randall@ost.ch', to_date('2020-05-05', 'YYYY-MM-DD')),
+(8, 'Sayed', 'Hendori', 'sayed.hendori@ost.ch', to_date('2020-07-12', 'YYYY-MM-DD'));
 
 INSERT INTO calendar values
 (1, 'Hausaufgaben', NULL),
--- dbs/5_queries.sql
+++ dbs/5_queries.sql
@@ -1,3 +1,89 @@
 /*
  * Authors: Oliver Dietsche, Simon Peier
  */
+
+-- 1.1 Eine Query mit DISTINCT
+-- Beschreibung: Alle Vornamen der Personen ohne Duplikate
+SELECT DISTINCT firstname as "unique firstnames"
+FROM person;
+
+-- 1.2 Eine Query, die einen JOIN über drei oder mehr Tabellen enthält
+-- Beschreibung: Namen aller Meetings, die Person mit ID = 3 in allen ihren Kalendern gemeinsam hat
+SELECT meet.name as "Meeting"
+FROM person pers
+INNER JOIN person_calendar pers_cal ON pers.id = pers_cal.person
+INNER JOIN calendar cal ON pers_cal.calendar = cal.id
+INNER JOIN meeting meet ON cal.id = meet.calendar
+WHERE pers.id = 3;
+
+-- 1.3 Eine Query mit einer Unterabfrage mit Angabe, ob diese Query korreliert ist oder unkorreliert
+-- Beschreibung: Vorname und Nachname der Personen, die 1 oder mehr Meetings erstellt haben
+SELECT firstname, lastname
+FROM person
+WHERE id IN ( -- Subquerry ist korreliert
+    SELECT scheduler
+    FROM meeting
+    GROUP BY scheduler
+    HAVING count(*) >= 2
+);
+
+-- 1.4. Eine Query, die eines der vier folgenden Statements enthält: ANY, IN, EXISTS oder ALL
+-- Beschreibung: Vorname und Nachname der Personen, die 1 oder weniger Meetings erstellt haben
+SELECT firstname, lastname
+FROM person
+WHERE id NOT IN (
+    SELECT scheduler
+    FROM meeting
+    GROUP BY scheduler
+    HAVING count(*) >= 2
+);
+
+-- 2.2.1 Unterabfrage im FROM-Teil (anstelle einer Tabelle)
+-- Beschreibung: Vorname und Nachname der Personen, die am meisten Meetings erstellt haben
+SELECT firstname, lastname
+FROM (
+	SELECT pers.firstname, pers.lastname,
+	rank() OVER (ORDER BY count(pers.id) DESC) as ranking
+	FROM person pers
+	INNER JOIN meeting meet ON pers.id = meet.scheduler
+    GROUP BY pers.id
+) AS pers
+WHERE ranking = 1;
+
+-- 2.2.2 Common Table Expression
+WITH scheduling_rank AS  (
+	SELECT pers.firstname, pers.lastname,
+	rank() OVER (ORDER BY count(pers.id) DESC) as ranking
+	FROM person pers
+	INNER JOIN meeting meet ON pers.id = meet.scheduler
+    GROUP BY pers.id
+)
+SELECT firstname, lastname
+FROM scheduling_rank
+WHERE ranking = 1;
+
+-- 2.2.1 Schreiben Sie eine sinnvolle Query mit einer GROUP BY-Klausel
+-- Beschreibung: Name des Kalenders mit der kleinsten Durchschnittsdauer der Meetings
+SELECT cal.name
+FROM meeting meet
+INNER JOIN calendar cal ON meet.calendar = cal.id
+GROUP BY meet.calendar, cal.id
+HAVING avg(meet.duration) <= ALL (
+    SELECT avg(duration)
+    FROM meeting
+    GROUP BY calendar
+);
+
+-- 2.2.2 Schreiben Sie eine sinnvolle Query mit einer Window-Funktion
+-- Beschreibung: Vorname und Nachname der Personen, die am meisten Meetings erstellt haben
+-- Bemerkung: count() als Window-Funktion, oder für dasselbe mit rank() siehe 2.1
+SELECT pers.firstname, pers.lastname
+FROM person pers
+INNER JOIN meeting meet ON pers.id = meet.scheduler
+GROUP BY pers.id
+HAVING count(pers.id) >= ALL (
+    SELECT count(pers.id)
+    FROM person pers
+    INNER JOIN meeting meet ON pers.id = meet.scheduler
+    GROUP BY pers.id
+);
